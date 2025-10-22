5ad582ffb5c58a5547fbfa0ce8b9286efe1479cc
Viktor Belyaev <va.beliaew@gmail.com>
2025-10-22 22:30:17 +0300

    Decoratorss

---
+++ Source/LeaveMeAlone/Core/Characters/LMAAIController.cpp
@@ -0,0 +1,5 @@
+// LeaveMeAlone game by Netologia. All rights reserved.
+
+
+#include "Core/Characters/LMAAIController.h"
+
---
+++ Source/LeaveMeAlone/Core/Characters/LMAAIController.h
@@ -0,0 +1,17 @@
+// LeaveMeAlone game by Netologia. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "LMAAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAVEMEALONE_API ALMAAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
